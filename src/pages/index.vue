e1817a55440dac079688b802ae7537882e5cb8d4
cenjingxiangshi <cenjingxiangshi@163.com>
2019-11-06 15:11:03 +0800

    "初始化“

---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index">
+    <h1>我是首页</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+@import "../assets/style/varibles.less";
+.index{
+   .zjw-center;
+    width: 200px;
+    height: 100px;
+  h1{
+   
+   
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import index from '@/pages/index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "index", 
+      redirect: '/index'
+    },
+    {
+      path: '/index',
+      name: 'index',
+      component: index
     }
   ]
 })
